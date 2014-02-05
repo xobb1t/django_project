84048f1f58e8e3ca5419aa7c61fe2410c1ee2b4b
Timofey Kukushkin <reebokey@me.com>
2014-02-06 01:15:13 +0400

    Update requirements

--- project/settings/development.py
+++ project/settings/development.py
@@ -7,18 +7,12 @@ DATABASES = {
     }
 }
 
-for i, middleware in enumerate(MIDDLEWARE_CLASSES):
-    if 'CommonMiddleware' in middleware:
-        mcs = list(MIDDLEWARE_CLASSES)
-        mcs.insert(i + 1, 'debug_toolbar.middleware.DebugToolbarMiddleware')
-        MIDDLEWARE_CLASSES = tuple(mcs)
-        INSTALLED_APPS += ('debug_toolbar',)
-        break
+INSTALLED_APPS += ('debug_toolbar',)
+
+INTERNAL_IPS = ('127.0.0.1',)
 
 DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': False,
 }
-
-INTERNAL_IPS = ('127.0.0.1',)
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
